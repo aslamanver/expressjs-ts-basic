a4dc3d8d1aef2c57799c70f23b4b3cf7cd8781c0
Aslam Anver <slam.anver9@gmail.com>
2019-12-09 19:09:51 +0530

    Template engine added

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,11 @@ import express = require('express');
 // Create a new express application instance
 const app: express.Application = express();
 
+// Initiating
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
 app.listen(3000, function () {
