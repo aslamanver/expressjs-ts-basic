33c8cd9082c2a4243c5605c0614aa79a975e9f22
Aslam Anver <slam.anver9@gmail.com>
2020-01-23 12:24:36 +0530

    MongoDB Database in Docker

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,31 @@
-// app/app.ts
-import express = require('express');
+// src/app.ts
+import express = require('express')
+import mongoose = require('mongoose')
+
+// Create MongoDB user and database to connect "mydb"
+// use mydb
+// db.createUser({ user: "user", pwd: "password", roles: [ { role: "readWrite", db: "mydb" } ], passwordDigestor : "server" })
+// mongoose.connect('mongodb://user:password@mongo:27017/mydb')s
+mongoose.connect('mongodb://root:password@mongo:27017/admin')
 
 // Create a new express application instance
-const app: express.Application = express();
+const app: express.Application = express()
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+
+db.once('open', function () {
+  console.log('Database connected.')
+})
 
 // Initiating
 app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
-});
+  res.render('index', { title: 'Hey', message: 'Hello there !' })
+})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  console.log('Example app listening on port 3000!')
+})
---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+node -v
+npm -v
+npm install
+npm run dev
+# apt-get install curl
+# curl -i https://google.lk
+# python --version
+# pwd
